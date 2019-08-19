f21d2b5aa6cd8555e8e610a67cb4b6f4501bd2d1
Alex Saxena <saxenaalex@gmail.com>
2019-08-20 00:17:53 +0200

    Pushing first Javascript exercises

---
+++ javascript_exercise_1/javascript_exe_2.js
@@ -0,0 +1,5 @@
+// Javascript exercise 1.3 Questions
+
+// 1. what data type is 123/12?                //Answer, Numbers
+// 2. what data type is "Things in quotes"?   //Answer, "strings"
+// 3. what data type is undefined?            //Answer, Undefined or "Value not assigned"
