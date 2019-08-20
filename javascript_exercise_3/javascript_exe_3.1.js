df39c4c52ffab80338bb35a12715955ecb214df7
Alex Saxena <saxenaalex@gmail.com>
2019-08-20 23:12:33 +0200

    Pushing javascript exe 3, but I aint 100% sure about this one chief

---
+++ javascript_exercise_3/javascript_exe_3.1.js
@@ -0,0 +1,4 @@
+//Javascript Exercise 3.1 This definition
+//What does "this" mean in Javascript
+//Answer: the "this" keyword is referencing the object that is executing the current function.
+//If you use "this" in a function it will refernce the Global object which is the Windows object.
